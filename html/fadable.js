620cf29633e03e9beeb583a4ad8789e474e649cf
Marc Amos <marc@theout.fit>
2019-05-03 16:28:50 -0400

    Wrap-up v2

--- html/fadable.js
+++ html/fadable.js
@@ -1,63 +1,27 @@
-var fadable = {
-
-  fadeDistance: 200,
-  fadeClass   : 'fadable--invisible',
-  vpHeight    : null,
-  fadeLine    : null,
-  fadables    : null,
-  resizeTimer : null,
-
-  initialize: function() {
-    fadable.inspectEnvironment();
-    fadable.selectFadables();
-    fadable.prepareFadables();
-    fadable.bindEvents();
-  },
-
-  inspectEnvironment: function() {
-    fadable.vpHeight = window.innerHeight;
-    fadable.fadeLine = fadable.vpHeight - fadable.fadeDistance;
-  },
-
-  selectFadables: function() {
-    fadable.fadables = document.querySelectorAll('.fadable');
-  },
-
-  prepareFadables: function() {
-    Array.prototype.forEach.call(fadable.fadables, function(el, i) {
-      if (el.getBoundingClientRect().top > fadable.vpHeight) {
-        el.classList.add(fadable.fadeClass);
+if ('IntersectionObserver' in window) {
+  let observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(el) {
+      if (el.intersectionRatio > 0) {
+        // Opacity and scale when the element is in view:
+        el.target.style.opacity = '1'
+        el.target.style.transform = 'scale(1.0)'
+      } else {
+        // Configure: Opacity and scale when the element is out of view:
+        el.target.style.opacity = '0'
+        el.target.style.transform = 'scale(0.95)'
+        // Configure: Animation duration:
+        el.target.style.transitionDuration = '0.75s, 0.75s'
+        el.target.style.transitionProperty = 'opacity, transform'
+        // Configure: Animation timing function:
+        el.target.style.transitionTimingFunction = 'ease-out, ease-out'
       }
-    });
-  },
-
-  showInvisibles: function() {
-    Array.prototype.forEach.call(fadable.fadables, function(el, i) {
-      if (el.getBoundingClientRect().top < fadable.fadeLine) {
-        el.classList.remove(fadable.fadeClass);
-      }
-    });
-  },
-
-  bindEvents: function() {
-    document.addEventListener('scroll', function() {
-      fadable.showInvisibles();
-    });
-
-    window.addEventListener('resize', function() {
-      clearTimeout(fadable.resizeTimer);
-      fadable.resizeTimer = setTimeout(function() {
-        fadable.inspectEnvironment();
-        fadable.prepareFadables();
-        fadable.showInvisibles();
-      }, 250);
-    });
-  }
-
-}
-
-document.onreadystatechange = function() {
-  if (document.readyState == 'complete') {
-    fadable.initialize();
-  }
+    })
+  }, {
+    // Configure: The distance from the browser viewport’s edges.
+    // The order is top edge, right edge, bottom edge, and left edge.
+    rootMargin: '-40px 0px -40px 0px'
+  })
+  document.querySelectorAll('.fadable').forEach(function(targetEl) {
+    observer.observe(targetEl)
+  })
 }
