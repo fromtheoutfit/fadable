7d1e33f2f36c3ac3b2e6d397bb484e03d7c41966
Marc Amos <marc@theout.fit>
2020-08-19 15:38:11 -0400

    Attempting to get demo page working…

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,19 @@ import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'src/fadable.js',
-    output: {
-        exports: 'default',
-        file: 'dist/fadable.min.js',
-        format: 'cjs',
-        plugins: [terser()]
-    }
+    output: [
+        {
+            exports: 'default',
+            file: 'dist/fadable.min.js',
+            format: 'cjs',
+            plugins: [terser()]
+        },
+        {
+            exports: 'default',
+            file: 'demo/fadable.min.js',
+            format: 'iife',
+            name: 'fadable',
+            plugins: [terser()]
+        }
+    ]
 };
